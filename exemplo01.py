85c3406c20aa5478f78221a3725b754270c91971
Dntsantos <filipe.donato@aluno.senac.rio>
2026-01-06 19:18:43 -0300

    aula11

--- exemplo01.py
+++ exemplo01.py
@@ -2,6 +2,15 @@
 import random
 
 # random.randint -> numero inteiros
-n = random.randint(1, 10)
-m = random.randint(1, 10)
-print(n, m)
+# n = random.randint(1, 10)
+# m = random.randint(1, 10)
+# print(n, m)
+
+# --------------------------------------------
+# Gerar numeros decimais
+n_decimal = random.uniform(1, 10)
+n_decimal_2 = random.uniform(1, 10)
+numero_decimal = round(n_decimal, 2) # arredonda para duas casas
+
+print(f'{n_decimal:.2f} e {n_decimal_2:.2f}') # formata . . . 
+print(numero_decimal)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+n_decimal = random.uniform(1, 10)
+n_decimal_2 = random.uniform(1, 10)
+print(n_decimal, n_decimal_2)
