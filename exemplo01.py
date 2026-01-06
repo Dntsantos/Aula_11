841373f9c8b0d4336473d6d42b7e5c7e2785c79d
Dntsantos <filipe.donato@aluno.senac.rio>
2026-01-06 19:47:45 -0300

    implementa a biblioteca ospython

--- exemplo01.py
+++ exemplo01.py
@@ -1,21 +1,23 @@
 # Bibliotecas
 import random
+import os
 
-random.randint # numero inteiros
-n = random.randint(1, 10)
-m = random.randint(1, 10)
-print(n, m)
+# random.randint # numero inteiros
+# n = random.randint(1, 10)
+# m = random.randint(1, 10)
+# print(n, m)
 
 # --------------------------------------------
 # Gerar numeros decimais
-n_decimal = random.uniform(1, 10)
-n_decimal_2 = random.uniform(1, 10)
-numero_decimal = round(n_decimal, 2) # arredonda para duas casas
+# n_decimal = random.uniform(1, 10)
+# n_decimal_2 = random.uniform(1, 10)
+# numero_decimal = round(n_decimal, 2) # arredonda para duas casas
 
-print(f'{n_decimal:.2f} e {n_decimal_2:.2f}') # formata . . . 
-print(numero_decimal)
+# print(f'{n_decimal:.2f} e {n_decimal_2:.2f}') # formata . . . 
+# print(numero_decimal)
 
 # ---------------------------------------------------------
 # Gerar numeros aleatorios dentro de uma lista
-lst_numeros = [random.randint(1, 10) for num in range (5)]
+lst_numeros = [random.randint(1, 1000) for num in range (5)]
+os.system('cls')
 print(lst_numeros)
