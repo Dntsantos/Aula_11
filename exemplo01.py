ae3e4b20421a44fb5cf0ebd481b6de235061d5cb
Dntsantos <filipe.donato@aluno.senac.rio>
2026-01-06 19:36:27 -0300

    gerar numeros aleatorios dentro de uma lista

--- exemplo01.py
+++ exemplo01.py
@@ -8,9 +8,14 @@ import random
 
 # --------------------------------------------
 # Gerar numeros decimais
-n_decimal = random.uniform(1, 10)
-n_decimal_2 = random.uniform(1, 10)
-numero_decimal = round(n_decimal, 2) # arredonda para duas casas
+# n_decimal = random.uniform(1, 10)
+# n_decimal_2 = random.uniform(1, 10)
+# numero_decimal = round(n_decimal, 2) # arredonda para duas casas
 
-print(f'{n_decimal:.2f} e {n_decimal_2:.2f}') # formata . . . 
-print(numero_decimal)
+# print(f'{n_decimal:.2f} e {n_decimal_2:.2f}') # formata . . . 
+# print(numero_decimal)
+
+# ---------------------------------------------------------
+# Gerar numeros aleatorios dentro de uma lista
+lst_numeros = [random.randint(1, 10) for num in range (5)]
+print(lst_numeros)
