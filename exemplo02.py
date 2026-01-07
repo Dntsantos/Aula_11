77177be26a9d8f4557f8d9dd364ea106f17c7660
Dntsantos <filipe.donato@aluno.senac.rio>
2026-01-06 21:49:07 -0300

    final

--- exemplo02.py
+++ exemplo02.py
@@ -6,11 +6,36 @@ def gerar_numeros(ini, fim, qtd):
     lst_num = [random.randint(ini, fim) for i in range (qtd)]
     return lst_num
 
+def somar_numeros(num1, num2):
+    sum = num1 + num2
+    return sum
+def sub_numeros(num1, num2):
+    sub = num1 - num2
+    return sub
+def mult_numeros(num1, num2):
+    mult = num1 * num2
+    return mult
+def div_numeros(num1, num2):
+    div = num1 / num2
+    return div
+
 # inicio do algoritmo
 inicio = 1
 final = 10
 quantidade = 2
-lst_numeros = gerar_numeros(inicio, final, quantidade)
+lst_numeros = gerar_numeros (inicio, final, quantidade)
+soma_numeros = sum 
+
+# lst_numeros = gerar_numeros(inicio, final, quantidade)
+
 print(lst_numeros[0])
 print(lst_numeros[1])
-print(lst_numeros)
+
+some = somar_numeros(lst_numeros[0], lst_numeros[1])
+print(some)
+sub = sub_numeros(lst_numeros[0], lst_numeros[1])
+print(sub)
+mult = mult_numeros(lst_numeros[0], lst_numeros[1])
+print(mult)
+div = div_numeros(lst_numeros[0], lst_numeros[1])
+print(div)
